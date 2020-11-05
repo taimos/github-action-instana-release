e069a2deaa1c022c8529e78eb2e7abd2fb414398
Michele Mancioppi <michele.mancioppi@gmail.com>
2020-11-05 11:02:01 +0100

    Make curl fail on 4xx and 5xx and show errors when it fails

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,9 @@ echo "Creating release $1"
 echo "${2}" > scope.json
 
 res=$(curl --location --request POST "${INSTANA_BASE}/api/releases" \
+  --silent \
+  --fail \
+  --show-error \
   --header "Authorization: apiToken ${INSTANA_TOKEN}" \
   --header "Content-Type: application/json" \
   --data "{
